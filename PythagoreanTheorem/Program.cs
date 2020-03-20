1f6c6795d3b3e9cd1d9a34684612d02fd97f7260
Enster <enster3@gmail.com>
2020-03-20 18:19:37 +0100

    Implemented Pythagorean theorem

--- PythagoreanTheorem/Program.cs
+++ PythagoreanTheorem/Program.cs
@@ -10,6 +10,13 @@ namespace PythagoreanTheorem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mekkora az a oldal?");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Mekkora az b oldal?");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("A 'c' oldal hossza: '");
+            Console.WriteLine(Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)));
+            Console.ReadKey();
         }
     }
 }
